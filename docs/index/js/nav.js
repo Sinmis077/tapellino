f4d49b6b7fe976e0d5cb28564dc8de55fae4ed44
Sinmis077 <llelga8@gmail.com>
2019-12-15 12:21:06 +0200

    update

---
+++ docs/index/js/nav.js
@@ -0,0 +1,8 @@
+function myFunction() {
+	var nav = document.getElementById("myTopnav");
+	if (nav.className === "topnav") {
+		nav.className += " responsive";
+	} else {
+		nav.className = "topnav";
+	}
+}
